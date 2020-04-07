6f4ebf23056b777ace3321e20c784746a764ce46
Matokosp <jmsalinas@uc.cl>
2020-04-07 15:30:19 -0400

    menu update scroll desktop

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -12,6 +12,9 @@ window.addEventListener('resize', () => {
 });
 
 
+// MENU
+
+
 var navButton = document.getElementById("navButton");
 var menuResponsive = document.getElementById("menu");
 var hambIconOne = document.getElementById("hambIconOne");
@@ -43,6 +46,91 @@ if (menuResponsive.classList) {
 navButton.onclick = displayNav;
 
 
+
+// MENU STYLE
+
+// if (document.location.pathname == "about.html") {
+//     console.log(document.URL);
+// }
+	var sectionOne = $('#section1').offset();
+	var sectionTwo = $('#section2').offset();
+	var position = $(window).scrollTop();
+
+	// should start at 0
+
+	$(window).scroll(function() {
+	    var scroll = $(window).scrollTop();
+	    if( scroll > position ) {
+	        console.log('scrollDown');
+	        $('.hamburger_div').removeClass('menu_responsive-show');
+	        $('.hamburger_div').addClass('menu_responsive-hide');
+	    }
+
+	    if( scroll < position ) {
+	    	$('.hamburger_div').removeClass('menu_responsive-hide')
+	    	$('.hamburger_div').addClass('menu_responsive-show');
+	        console.log('scrollUp');
+	    }
+
+      if( $('#menu').hasClass('menu_show') ) {
+        if( scroll < position ) {
+  	    	$('.hamburger_div').removeClass('menu_responsive-hide')
+  	    	$('.hamburger_div').addClass('menu_responsive-show');
+  	    }
+      }
+	    // else {
+	    //      console.log('scrollUp');
+	    // }
+	    position = scroll;
+	});
+
+	var x = window.matchMedia("(min-width: 1200px)")
+
+	function myFunction(x) {
+	  if (x.matches) { // If media query matches
+
+	  		var windowHeight = $( window ).height();
+	  		var position = $(window).scrollTop();
+	  		// SCROLL UP & DOWN
+		    $(window).scroll(function() {
+			    var scroll = $(window).scrollTop();
+			    if( scroll > position ) {
+			        $('.menu').addClass('menu_scroll');
+			    }
+			    if( scroll < position ) {
+			    	$('.menu').removeClass('menu_scroll');
+			    }
+			    // else {
+			    //      console.log('scrollUp');
+			    // }
+			    position = scroll;
+			});
+
+			$(window).scroll(function(){
+				if ( $(window).scrollTop() >= sectionTwo.top * 1 / 2) {
+			    	$('.main_menu').addClass('main_menu-white');
+			    	$('.main_menu-link').css('color', '#000');
+			    	$('.corner_logo').children('img:nth-child(1)').css('display', 'inline');
+			    	$('.corner_logo').children('img:nth-child(2)').css('display', 'none');
+			    } else {
+			    	$('.main_menu').removeClass('main_menu-white');
+			    	$('.main_menu-link').css('color', '#fff');
+			    	$('.main_menu').removeClass('main_menu-hide');
+			    	$('.corner_logo').children('img:nth-child(1)').css('display', 'none');
+			    	$('.corner_logo').children('img:nth-child(2)').css('display', 'inline');
+			    }
+			})
+	  }
+	}
+
+	myFunction(x) // Call listener function at run time
+	x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+
+
+// LOOP TEXT
 
 let y = document.querySelectorAll('.quote_one');
         y[0];
